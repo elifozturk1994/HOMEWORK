9ce74ed48ee0cfab25b0aa37413f9638e806f67d
Elif Öztürk <elifo3725@gmail.com>
2024-11-06 21:24:00 +0200

    Add files via upload
    C# Değişkenler ve Veri Tipleri Ödevleri- Soru:1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ödev01;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int sayi1 = 25;
+        int sayi2 = 40;
+        System.Console.WriteLine(sayi1 + sayi2);
+    }
+}
