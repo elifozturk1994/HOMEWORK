60fd82a5db979c000742ad4e800c43da9ef70802
Elif Öztürk <elifo3725@gmail.com>
2024-11-06 21:51:02 +0200

    C# Değişkenler ve Veri Tipleri Ödevleri
    bool tipinde iki değişken oluşturun ve bunlara true ve false değerlerini atayın. Bu değişkenleri ekrana yazdırın.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
-﻿namespace ödev04;
+﻿namespace ödev05;
 
 class Program
 {
     static void Main(string[] args)
     {
-        char harf = 'D';
-        int askideger = Convert.ToInt32(harf);
-        System.Console.WriteLine(askideger);
+        bool yazi = true;
+        bool yazi2 = false;
+        System.Console.WriteLine(yazi);
+        System.Console.WriteLine(yazi2);
     }
 }
